85f800b2665010b666024f77233118434bb5cb33
unknown <fouedtrabelsu2002@gmail.com>
2024-07-27 10:12:59 +0100

    client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,16 +3,18 @@ import Home from "./Pages/Home"
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Approved_Validator from "./Pages/Approved_Validator";
 import Login from "./Pages/login";
+import Certif_page from "./Pages/Certif_page";
 function App() {
   return (
     <>
-    <BrowserRouter>
+
     <Routes>
     <Route path='/' element={<Home />} />
+    <Route path='/About' element={<Certif_page />} />
      <Route path="/login" element={<Login />} />
       <Route path='/Home' element={<Approved_Validator />} />
     </Routes>
-    </BrowserRouter>
+    
     </>
   )
 }
